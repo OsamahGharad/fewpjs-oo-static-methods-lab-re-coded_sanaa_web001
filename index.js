d986f60382927d7e38039677beec815a97d47435
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-25 14:20:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 class Formatter {
-  //add static methods here
-  static capitalize(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+    //add static methods here
+    static capitalize(string){
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+  
+    static sanitize(string){
+   return string.replace(/[^A-Za-z0-9-']+/g, '');
+    }
+  
+    static titleize(string){
 
-  static sanitize(string){
- return string.replace(/[^A-Za-z0-9-']+/g, '');
+        let words =string.split(' ');
+        let exceptions =['a','an','the','but','of','by','at','from'];
+       let  newString="";
+        words.forEach(word =>{
+            if(words.indexOf(word)===0){
+                newString+=Formatter.capitalize(word);
+            }
+            else if(exceptions.includes(word)){
+                newString+=` ${word}`;
+            }
+            else {
+                newString+=' '+Formatter.capitalize(word);
+            }
+        })
+          return newString;
+      
+  
   }
-
 }
+  console.log(Formatter.titleize(" this is to test it if but a from yemen "))
